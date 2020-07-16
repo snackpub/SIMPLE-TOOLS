fd7ab03a512cbbbc3c58987e109ad78c44c398c6
snackpub <1498851306@qq.com>
2020-07-16 19:14:30 +0800

    将zkpkin配置集成到snack-core-boot中，补充新增表sql，将zkpkin的配置配置到snackpub.yml 统一配置

--- doc/sql/snack-seata.sql
+++ doc/sql/snack-seata.sql
@@ -62,3 +62,44 @@ CREATE TABLE `undo_log`
 -- 初始化库存模拟数据
 INSERT INTO seata_storage.tb_storage (id, commodity_code, count) VALUES (1, 'product-1', 9999999);
 INSERT INTO seata_storage.tb_storage (id, commodity_code, count) VALUES (2, 'product-2', 0);
+
+
+-- ----------------------------
+-- Table structure for tb_bank
+-- ----------------------------
+-- 创建 bankaccount库、业务表、undo_log表
+create database seata_bankaccount;
+use seata_bankaccount;
+DROP TABLE IF EXISTS `tb_bank`;
+CREATE TABLE `tb_bank` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `bank_account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `account_balance` decimal(20,2) DEFAULT NULL,
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of tb_bank
+-- ----------------------------
+INSERT INTO `tb_bank` VALUES ('1', '3031', '6920615594014', '70.00', '29');
+
+-- ----------------------------
+-- Table structure for undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
