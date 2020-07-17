1aeb749a04c1f4e4d250346973bbe0651d4fd5fa
snackpub <1498851306@qq.com>
2020-07-17 08:46:58 +0800

    提交pom

---
+++ snack-seata-bank/src/test/java/com/snackpub/SnackSeataBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snackpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackSeataBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
